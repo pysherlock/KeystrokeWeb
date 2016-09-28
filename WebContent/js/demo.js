6b00a8a41250bf9562914370a8d13a9b79d4f1c0
PU Yang <pu_yang@outlook.com>
2016-09-28 17:25:07 +0200

    0.6 Turn to Keystroke Extraction

--- WebContent/js/demo.js
+++ WebContent/js/demo.js
@@ -2,26 +2,59 @@
  *
  */
 
-var keystroke = function () {
+var Username = new Array(); Password = new Array(); // records the keys that user types in
+
+var keystroke = function (field) {
 	//keystroke event
+	var hold_time, DDKL, UDKL, UUKL; //keystroke features: hold-down time, latency between successive keys
+	var key = null, prev_key = null;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          ;
+	var KD_time, prev_KD_time, KU_time, prev_KU_time;
+	var Text = new Array();
 	
-	$("input").keydown(function(event) {
+	//Key press event
+	$(field).keypress(function(event) {
 		var keydown = event;
-		var keydown_time = keydown.timeStamp;
 		console.log(keydown);
+		
+		prev_key = key;
+		prev_KD_time = KD_time;
+		key = keydown.key;
+		KD_time = keydown.timeStamp;
 
-		$("input").keyup(function(event) {
-			var keyup = event;
-			var keyup_time = keyup.timeStamp;
-			var interval = keyup_time - keydown_time;
-			document.getElementById("output").innerHTML="keydown: " + keydown_time + "\n keyup_time: " + keyup_time + 
-										"  interval: " + interval;
-
-		});
-//		document.getElementById("output").innerHTML="keydown: " + keydown_time + "/nkeyup_time: " + keyup_time;
+		Text.push(key);
+	});
 	
+	//Key up event
+	$(field).keyup(function(event) {
+		var keyup = event;
+		console.log(keyup);
+		prev_KU_time = KU_time;
+		KU_time = keyup.timeStamp;
+		console.log(prev_KU_time, KU_time);
+		hold_time = KU_time - KD_time;
+		
+		if(prev_key != null) {
+			DDKL = KD_time - prev_KD_time;
+			UDKL = KU_time - prev_KD_time;
+			UUKL = KU_time - prev_KU_time;
+			document.getElementById("output").innerHTML="Succesive keys: " + prev_key + "," + key  
+												+ " KD (hold time): " + hold_time + " DDKL: " + DDKL 
+												+ " UDKL: " + UDKL + " UUKL: " + UUKL;
+		}
+	});
+	
+	return Text;
+	
+}
+
+var main = function () {
+	Username = keystroke("#username");
+	Password = keystroke("#password");
+	$('#triger').click(function() {
+		alert('Username: ' + Username + " Password: " + Password);
 	});
 	
 }
 
-$(document).ready(keystroke);
+$(document).ready(main);
+//$(document).ready(keystroke);
