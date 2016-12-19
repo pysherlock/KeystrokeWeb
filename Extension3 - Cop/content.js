7aa19f8554a46cdef2f7b9c817e242c7a7f6ee1f
PU Yang <pu_yang@outlook.com>
2016-12-19 17:45:01 +0100

    Bug_Fix to Keystroke Collector

--- Extension3 - Cop/content.js
+++ Extension3 - Cop/content.js
@@ -5,13 +5,8 @@ var city=""; var country_name=""; var ip=""; var latitude=""; var longitude=""; 
 var zip_code=""; var network=""; var cpuName=""; var os=""; var batteryCharging=""; var cap=""; var storage="";
 
 
-var Username_text = new Array(), Password_text = new Array(); // records the keys that user types in
 
-var KD_time_username = new Array(), KU_time_username = new Array();
-var KD_time_pwd = new Array(), KU_time_pwd = new Array();
-var Feature_Username = new Array(), Feature_Password = new Array(), Feature_Keystroke = new Array(), Keyevents_JSON = new Array();
-
-var index_password = -1, index_username = -1;
+// Keystroke Collector Part
 
 function KEY(index, key, which, downtime, uptime) {
 	this.index = index;
@@ -21,27 +16,31 @@ function KEY(index, key, which, downtime, uptime) {
 	this.time_U = uptime;
 }
 
-//Record the key which is being pressed and has been released
-var Pressed_Key = new Array(), Released_Key = new Array();
+function PressedIndex(index) {
+    this.index = index;
+}
 
-var keystroke = function(field, Text, Index) {
-	//Record the key which is being pressed and has been released
-//	var Pressed_Key = new Array(), Released_Key = new Array();
-	var Key;
-	
+var Text = new Array(); // records the keys that user types in
+//Record the key which is being pressed and has been released
+var PressedKey = new Array();
+var Feature_Keystroke = new Array();
+var PressedIndex = new PressedIndex(-1);
+var Key;
+
+
+var keystroke = function(field, text, pressedindex, pressed_key, released_key) {
+	//Record the key which is being pressed and has been released	
 	$(field).keydown(function(event) {
-		Index += 1;
-		Key = new KEY(Index, event.key, event.which, event.timeStamp, null);
-		Pressed_Key.push(Key);
-		
-		//console.log("Press: " + Key.key);
-		Text.push(event.key);
+		pressedindex.index += 1;
+		Key = new KEY(pressedindex.index, event.key, event.which, event.timeStamp, null);
+		pressed_key.push(Key);
+        text.push(event.key);
 	});
 	
 	$(field).keyup(function(event) {
-		for (i = 0; i < Pressed_Key.length; i++) {
+		for (i = 0; i < pressed_key.length; i++) {
 			//console.log("Pressed_Key.length: " + Pressed_Key.length + " i: " + i);
-			item = Pressed_Key[i];
+			item = pressed_key[i];
 			
 			//Look for the same key in Pressed key array and release them
 			//Deal with capital keys: delete the upper key from the Pressed array
@@ -51,12 +50,12 @@ var keystroke = function(field, Text, Index) {
 				
 				//Release the key
 				//Add released key to Released array
-				Released_Key.push(item);
+				released_key.push(item);
 				//console.log("Released_Key");
 				//console.log(Released_Key);
 
 				//Delete released key from the Pressed array
-				Pressed_Key.splice(i, 1);
+				pressed_key.splice(i, 1);
 				//console.log("Pressed_Key");
 				//console.log(Pressed_Key);
 				
@@ -64,7 +63,7 @@ var keystroke = function(field, Text, Index) {
 			}
 		}
 	});
-	return Released_Key;
+//	return Released_Key;
 }
 
 $(document).click(function(event) {
@@ -110,90 +109,91 @@ storage=message.response2[16];}
 });});
   
 function myFunction(){
-  //var port = chrome.runtime.connect({name:"mycontentscript"});
-  port.postMessage({hi:"hi"})
-  port.onMessage.addListener(function(message,sender){
-  var chromeVersion = /Chrome\/([0-9.]+)/.exec(navigator.userAgent)[1];
-  var appName = navigator.appName
-  var b=true;
-  var Now=new Date();
-  var NowDate=(Now.getTime());
-  var Diff=NowDate-message.response[4];
-  if (Diff > 15000 ) {b=false};
-  var c=true;
-  var Diff2=NowDate-message.response[12];
-  if (Diff2 > 15000 ) {c=false};
-  var g=true;
-  var Diff3=NowDate-message.response[13];
-  if (Diff3 > 15000 ) {g=false};
-  
-  var j=true;
-  var Diff4=NowDate-message.response[14];
-  if (Diff4 > 15000 ) {j=false};
-  
-  var m=true;
-  var Diff5=NowDate-message.response[15];
-  if (Diff5 > 15000 ) {m=false};
-  
-  var volume=true;
-  var Diff6=NowDate-message.response[16];
-  if (Diff6 > 15000 ) {volume=false};
-  
-  var pause=true;
-  var Diff7=NowDate-message.response[17];
-  if (Diff7 > 15000 ) {pause=false};
-  
-  var speed=true;
-  var Diff8=NowDate-message.response[18];
-  if (Diff8 > 15000 ) {speed=false};
-  
-  var mute=true;
-  var Diff9=NowDate-message.response[19];
-  if (Diff9 > 15000 ) {mute=false};
-  
-  var end=true;
-  var Diff10=NowDate-message.response[22];
-  if (Diff10 > 15000 ) {end=false};
-  
-  var see=true;
-  var Diff11=NowDate-message.response[23];
-  if (Diff11 > 15000 ) {see=false};
-  
-  var Keyevents_JSON = new Array();
-  for(i = 0; i < Feature_Keystroke.length; i++) {
-      var str = '{' + '"index":' + Feature_Keystroke[i].index + ',"key":' + '"' + Feature_Keystroke[i].key + '"' +
+    //var port = chrome.runtime.connect({name:"mycontentscript"});
+    port.postMessage({hi:"hi"})
+    port.onMessage.addListener(function(message,sender){
+    var chromeVersion = /Chrome\/([0-9.]+)/.exec(navigator.userAgent)[1];
+    var appName = navigator.appName
+    var b=true;
+    var Now=new Date();
+    var NowDate=(Now.getTime());
+    var Diff=NowDate-message.response[4];
+    if (Diff > 15000 ) {b=false};
+    var c=true;
+    var Diff2=NowDate-message.response[12];
+    if (Diff2 > 15000 ) {c=false};
+    var g=true;
+    var Diff3=NowDate-message.response[13];
+    if (Diff3 > 15000 ) {g=false};
+
+    var j=true;
+    var Diff4=NowDate-message.response[14];
+    if (Diff4 > 15000 ) {j=false};
+
+    var m=true;
+    var Diff5=NowDate-message.response[15];
+    if (Diff5 > 15000 ) {m=false};
+
+    var volume=true;
+    var Diff6=NowDate-message.response[16];
+    if (Diff6 > 15000 ) {volume=false};
+
+    var pause=true;
+    var Diff7=NowDate-message.response[17];
+    if (Diff7 > 15000 ) {pause=false};
+
+    var speed=true;
+    var Diff8=NowDate-message.response[18];
+    if (Diff8 > 15000 ) {speed=false};
+
+    var mute=true;
+    var Diff9=NowDate-message.response[19];
+    if (Diff9 > 15000 ) {mute=false};
+
+    var end=true;
+    var Diff10=NowDate-message.response[22];
+    if (Diff10 > 15000 ) {end=false};
+
+    var see=true;
+    var Diff11=NowDate-message.response[23];
+    if (Diff11 > 15000 ) {see=false};
+
+    var Keyevents_JSON = new Array();
+    for(i = 0; i < Feature_Keystroke.length; i++) {
+        var str = '{' + '"index":' + Feature_Keystroke[i].index + ',"key":' + '"' + Feature_Keystroke[i].key + '"' +
                 ',"which":' + Feature_Keystroke[i].which + ',"time_D":' + Feature_Keystroke[i].time_D +
                 ',"time_U":' + Feature_Keystroke[i].time_U + '}';
-      if(i == 0) {
-          Keyevents_JSON.push('"keystroke":[' + str);
-      }
-      else if(i == (Feature_Keystroke.length - 1)) {
-          Keyevents_JSON.push(str+']');
-      }
-      else {
-          Keyevents_JSON.push(str);
-      }
-  }
+        if(i == 0) {
+            Keyevents_JSON.push('"keystroke":[' + str);
+        }
+        else if(i == (Feature_Keystroke.length - 1)) {
+            Keyevents_JSON.push(str+']');
+        }
+        else {
+            Keyevents_JSON.push(str);
+        }
+    }
 
-  var Keystroke_JSON = '{' + Keyevents_JSON.toString() + '}';
+    var Keystroke_JSON = '{' + Keyevents_JSON.toString() + '}';
 
-      
-  var text = "";
+    var text = "";
     if (window.getSelection) {
-        text = window.getSelection().toString();}
-		
+        text = window.getSelection().toString();}		
  
-  if (typeof position == 'undefined') {position=0};
-  var  dat={processor:cpuName, os:os, memory:cap, chrome:chromeVersion, storage:storage,
-     locIP:network, pubIP:ip, country:country_name, region: region_name,zip:zip_code,
-	 charge:batteryCharging, tabIndex:message.response[8], removable:b, transition:message.response[0], priv:message.response[5],
-	 width: message.response[6], height:message.response[7], url:message.response[9], press:presstime, zoom:message.response[10],
-	 click:position, selected:text, lastCharacter:key, detach:c, closetab:g, bookmarkChange:j, volumeChange:volume, volume: message.response[21],
-	 muted:mute, paused:pause, speedChange:speed, currentTime:message.response[20], ended:end, seek:see, keystroke:Keystroke_JSON};
+    if (typeof position == 'undefined') {position=0};
+    var dat={processor:cpuName, os:os, memory:cap, chrome:chromeVersion, storage:storage,
+        locIP:network, pubIP:ip, country:country_name, region: region_name,zip:zip_code,
+        charge:batteryCharging, tabIndex:message.response[8], removable:b, transition:message.response[0], priv:message.response[5],
+        width: message.response[6], height:message.response[7], url:message.response[9], press:presstime, zoom:message.response[10],
+        click:position, selected:text, lastCharacter:key, detach:c, closetab:g, bookmarkChange:j, volumeChange:volume, volume: message.response[21],
+        muted:mute, paused:pause, speedChange:speed, currentTime:message.response[20], ended:end, seek:see, keystroke:Keystroke_JSON};
 //data=message.response[0]
 // send data through a DOM event
-document.dispatchEvent(new CustomEvent('csEvent', {detail: dat}));
-Released_Key.splice(0, Released_Key.length);
+
+    document.dispatchEvent(new CustomEvent('csEvent', {detail: dat}));
+    Feature_Keystroke.splice(0, Feature_Keystroke.length);
+    Text.splice(0, Text.length);
+    PressedIndex.index = -1;
 });}
 
 /*window.onload = function () { 
@@ -205,9 +205,8 @@ element.addEventListener("click", myFunction, false);
 */
 $(document).ready(function() { start();});
 
-
 function start() {
-	 Feature_Keystroke = keystroke("#officeIdInput", Username_text, index_password);
+    keystroke("#officeIdInput", Text, PressedIndex, PressedKey, Feature_Keystroke);
 	 var element = (document.getElementsByClassName("button"))[2];
      if (typeof element == 'undefined') {
        window.requestAnimationFrame(start);
--- WebContent/js/keystroke.js
+++ WebContent/js/keystroke.js
@@ -2,14 +2,6 @@
  *
  */
 
-var Username_text = new Array(), Password_text = new Array(); // records the keys that user types in
-
-var KD_time_username = new Array(), KU_time_username = new Array();
-var KD_time_pwd = new Array(), KU_time_pwd = new Array();
-var Feature_Username = new Array(), Feature_Password = new Array();
-
-var index_password = -1, index_username = -1;
-
 function KEY(index, key, which, downtime, uptime) {
 	this.index = index;
 	this.key = key;
@@ -18,58 +10,59 @@ function KEY(index, key, which, downtime, uptime) {
 	this.time_U = uptime;
 }
 
-//Record the key which is being pressed and has been released
-var Pressed_Key = new Array(), Released_Key = new Array();
+function PressedIndex(index) {
+    this.index = index;
+}
 
-var keystroke = function(field, Text, Index) {
+var Username_text = new Array(), Password_text = new Array(); // records the keys that user types in
+//Record the key which is being pressed and has been released
+var UsernamePressed_Key = new Array(), PasswordPressed_Key = new Array();
+var Feature_Username = new Array(), Feature_Password = new Array();
+var index_password = new PressedIndex(-1), index_username = new PressedIndex(-1);
+var Key;
+
+var keystroke = function(field, Text, Index, Pressed_Key, Released_Key) {
 	//Record the key which is being pressed and has been released
-//	var Pressed_Key = new Array(), Released_Key = new Array();
-	var Key;
-	
 	$(field).keydown(function(event) {
-		Index += 1;
-		Key = new KEY(Index, event.key, event.which, event.timeStamp, null);
+		Index.index += 1;
+		Key = new KEY(Index.index, event.key, event.which, event.timeStamp, null);
 		Pressed_Key.push(Key);
-		
-		console.log("Press: " + Key.key);
 		Text.push(event.key);
 	});
 	
 	$(field).keyup(function(event) {
 		for (i = 0; i < Pressed_Key.length; i++) {
-			console.log("Pressed_Key.length: " + Pressed_Key.length + " i: " + i);
 			item = Pressed_Key[i];
 			
 			//Look for the same key in Pressed key array and release them
 			//Deal with capital keys: delete the upper key from the Pressed array
 			if(item.which == event.which || item.which == event.which+32 || item.which == event.which-32) {
 				item.time_U = event.timeStamp;
-				console.log("Release: " + item.key);
-				
+//				console.log("Release: " + item.key);
+
 				//Release the key
 				//Add released key to Released array
 				Released_Key.push(item);
-				console.log("Released_Key");
-				console.log(Released_Key);
+//				console.log("Released_Key");
+//				console.log(Released_Key, Feature_Username, Feature_Password);
 
 				//Delete released key from the Pressed array
 				Pressed_Key.splice(i, 1);
-				console.log("Pressed_Key");
-				console.log(Pressed_Key);
+//				console.log("Pressed_Key");
+//				console.log(Pressed_Key);
 				
 				i--; //correct the index position since Pressed array's length reduce one 
 			}
 		}
 	});
-	//return text;
-	return Released_Key;
+//	return Released_Key;
 }
 
 var main = function () {
 	
-	Feature_Username = keystroke("#username", Username_text, index_password);
-	Feature_Password = keystroke("#password", Password_text, index_username);
-	
+	keystroke("#username", Username_text, index_username, UsernamePressed_Key, Feature_Username);
+	keystroke("#password", Password_text, index_password, PasswordPressed_Key, Feature_Password);
+
 	var connection = new WebSocket("ws://localhost:8000/websocket");
 	connection.onopen = function() {
    		connection.send("Hello, world");
@@ -77,11 +70,11 @@ var main = function () {
 	connection.onmessage = function (evt) {
    		alert(evt.data);
    		if(evt.data.indexOf("True") != -1){
-   		    console.log("here");
+//   		    console.log("here");
    		    document.getElementById("p1").innerHTML = "Authentication succeeded or you cracked this: " + evt.data;
    		}
    		else {
-   		    console.log("here");
+//   		    console.log("here");
             document.getElementById("p1").innerHTML = "Authentication failed. Who are you or are you injured? " + evt.data;
         }
 	};
@@ -108,7 +101,6 @@ var main = function () {
 		        Feature_Password_JSON.push('"password":' + '"' + password +'"' + ',"keyevent":[' + str);
 		    }
 		    else if(i == (Feature_Password.length - 1)) {
-		        console.log(i);
 		        Feature_Password_JSON.push(str+']');
 		    }
 		    else {
@@ -146,9 +138,12 @@ var main = function () {
 
 		connection.send(Feature_JSON);
 
-	    Released_Key.splice(0, Released_Key.length);
+	    Feature_Password.splice(0, Feature_Password.length);
+	    Feature_Username.splice(0, Feature_Username.length);
 	    Username_text.splice(0, Username_text.length);
 	    Password_text.splice(0, Password_text.length);
+	    index_password.index = -1; index_username.index = -1;
+	    console.log(index_username, index_password);
 	});
 }
 
